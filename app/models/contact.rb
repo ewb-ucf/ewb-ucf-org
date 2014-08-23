4cf91dd14ced5d29a90e15e512cd6ac18eecfc97
mckinnley <mck.workman@gmail.com>
2014-08-22 21:19:25 -0400

    users, teams, people, etc all functional

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -9,7 +9,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "My Contact Form",
+      :subject => "Website Inquiry",
       :to => "ucf.ewb.president@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
